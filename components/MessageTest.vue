d5e29965570ea98cda95f12eba36be2297b0a8aa
Arturo Farias <pedro.farias.sosa@gmail.com>
2024-10-20 23:41:17 -0600

    Feat: Add modules for projects

---
+++ components/MessageTest.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Mensaje de prueba: {{ message }}</h1>
+</template>
+
+<script setup>
+const props = defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <MessageTest message="Hola desde el test" />
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
